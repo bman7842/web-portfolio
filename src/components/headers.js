1b59ee2b6e9eb67a20d293b2c908fd0e2d692388
Brandon <brandonegger64@gmail.com>
2022-05-01 14:00:35 -0500

    Minor improvements to drop down menu components

--- src/components/headers.js
+++ src/components/headers.js
@@ -9,7 +9,7 @@ const NavButton = (props) => {
 
     return (
         <Link to={linkTo}>
-            <div class={"py-2 px-2 font-semibold " + (selected===label ? "border-b-4 border-cyan-800 text-cyan-700" : "text-gray-500 hover:text-cyan-700 transition duration-300")}>
+            <div class={"py-1 px-2 font-semibold border-cyan-800 " + (selected===label ? "border-b-4 text-cyan-700" : "hover:border-dotted hover:border-b-4 text-gray-500 hover:text-cyan-700 transition duration-300")}>
                 {props.children}
             </div>
         </Link>
@@ -25,15 +25,17 @@ const DropDownNavButton = (props) => {
     return (
         <div class="relative group">
             <Link class="w-full h-full" to={linkTo}>
-                <div class={"px-2 py-2 mt-2 md:w-auto md:inline mt-0 font-semibold "+ (selected===label ? "border-b-4 border-cyan-800 text-cyan-700" : "text-gray-500 transition duration-300 hover:text-cyan-700")}>
+                <div class={"py-1 px-2 font-semibold border-cyan-800 " + (selected===label ? "border-b-4 text-cyan-700" : "group-hover:border-dotted group-hover:border-b-4 text-gray-500 hover:text-cyan-700 transition duration-300")}>
                     {text}
                 </div>
             </Link>
-            <div class="absolute z-10 hidden bg-gray-200 group-hover:block">
-                <div class="w-32 rounded p-2 pb-2 bg-white bg-gray-200 shadow-lg">
-                <ul class="grid place-items-center grid-cols-1 gap-2">
-                    {props.children}
-                </ul>
+            <div class="absolute left-0 right-0 hidden justify-center overflow-visible z-10 group-hover:flex">
+                <div>
+                    <div class="w-32 rounded px-2 py-2 bg-gray-200 shadow-lg border border-cyan-800">
+                        <ul class="grid place-items-center grid-cols-1 divide-y divide-dashed">
+                            {props.children}
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
@@ -46,7 +48,7 @@ const DropDownNavSubButton = (props) => {
     const linkTo = props.to;
 
     return (
-        <div class="w-full rounded transition duration-300 hover:bg-gray-500 text-center">
+        <div class="w-full rounded transition duration-300 hover:bg-gray-400 text-center last:border-b-0 border-slate-400">
             <Link to={linkTo}><div class={"hover:text-white "+(subcat===label ? "text-cyan-800" : "")}>{props.children}</div></Link>
         </div>
     )
@@ -61,6 +63,7 @@ const NavBar = (props) => {
             <nav class="flex items-center space-x-2">
                 <NavButton to="/" label="home" selected={selected}>Home</NavButton>
                 <DropDownNavButton to="/projects/" label="projects" selected={selected} text="Projects">
+                    <DropDownNavSubButton to="" label="doogle" selected={selected} subcat={subcat}>doogle</DropDownNavSubButton>
                     <DropDownNavSubButton to="/projects/memorizer/" label="memorizer" selected={selected} subcat={subcat}>memorizer</DropDownNavSubButton>
                 </DropDownNavButton>
                 <NavButton to="/portfolio/" label="portfolio" selected={selected}>Portfolio</NavButton>
