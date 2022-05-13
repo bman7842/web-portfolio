90a23bf789c65ec0a2adb0401c518f4112e31a93
Brandon Egger <begger@Brandons-MacBook-Pro.local>
2022-05-13 12:52:28 -0500

    Improved notepad rendering

--- src/components/notepad.js
+++ src/components/notepad.js
@@ -4,6 +4,14 @@ const linedBackgroundStyle = {
     background: `repeating-linear-gradient(white, white 25px, #2F4F4F 26px, #A9A9A9 27px)`
 }
 
+const CollapsibleArrow = ({ collapsed, onClick }) => {
+    return (
+        <button onClick={onClick} className="absolute right-0 top-0 left-0 bottom-0 w-full opacity-50 transition ease-in hover:opacity-100">
+            <span className="absolute right-2 top-0">{collapsed ? "▼" : "▲"}</span>
+        </button>
+    )
+}
+
 /**
  * A simple notepad design, can be lined or unlined and collapsible or uncollapsible.
  * @param {*} props 
@@ -23,16 +31,6 @@ const Notepad = ({ collapsible, lined, header, startCollapsed, children }) => {
         setCollapsed(!collapsed);
     }
 
-    const CollapsibleArrow = () => {
-        const arrow = collapsed ? "▼" : "▲";
-
-        return (
-            <button onClick={handleCollapse} className="absolute right-0 top-0 left-0 bottom-0 w-full opacity-50 transition ease-in hover:opacity-100">
-                <span className="absolute right-2 top-0">{arrow}</span>
-            </button>
-        )
-    }
-
     return (
         <div className="w-full shadow-slate-600 shadow-sm flex-col text-center justify-items-center overflow-hidden bg-white rounded-xl border-black border">
             <div className= "relative w-full bg-amber-200/50">
@@ -42,7 +40,7 @@ const Notepad = ({ collapsible, lined, header, startCollapsed, children }) => {
                     </span>
                 </h1>
                 {collapsible &&
-                    <CollapsibleArrow />
+                    <CollapsibleArrow collapsed={collapsed} onClick={handleCollapse} />
                 }
             </div>
             <div style={ lined ? linedBackgroundStyle : {} } className={collapsed ? "hidden" : ""}>
